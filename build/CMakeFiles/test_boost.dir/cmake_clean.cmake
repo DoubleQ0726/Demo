47331e55bea250ca59f202c78a89ecba76a6ed14
DoubleQ <1156970769@qq.com>
2022-07-31 20:02:35 +0800

    Add cpp test demo

---
+++ build/CMakeFiles/test_boost.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "../bin/test_boost"
+  "../bin/test_boost.pdb"
+  "CMakeFiles/test_boost.dir/src/test_boost.cpp.o"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/test_boost.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ cmake/utils.cmake
@@ -0,0 +1,3 @@
+function(DailyDemo_Add_Executable targetname srcs)
+    add_executable(${targetname} ${srcs})
+endfunction()
